5e50a8efec996b5db00787c5e406071f1cd455d3
nivi.mor <nivi.mor@experitest.com>
2017-11-16 15:16:23 +0200

    added support for exit code in runner

--- test_runner.py
+++ test_runner.py
@@ -1,14 +1,14 @@
 import unittest
 from concurrent.futures import ThreadPoolExecutor
-import ios_web_test
 import android_web_test
 import android_app_test
+import ios_web_test
 import ios_app_test
+import sys
 
 
 def parallel_execution(self, *tests):
     suite = unittest.TestSuite()
-
     for test in tests:
         suite.addTest(unittest.TestLoader().loadTestsFromTestCase(test))
 
@@ -16,11 +16,18 @@ def parallel_execution(self, *tests):
         list_of_suites = list(suite)
         for test in range(len(list_of_suites)):
             test_name = str(list_of_suites[test])
-            executor.submit(unittest.TextTestRunner().run, list_of_suites[test])
+            return executor.submit(unittest.TextTestRunner().run, list_of_suites[test])
 
 
-parallel_execution(0,
-                   ios_app_test.IosAppTest,
-                   android_app_test.AndroidAppTest,
-                   android_web_test.TestWebsiteAndroidChrome,
-                   ios_web_test.TestWebsiteiOSSafari)
+res = parallel_execution(0, ios_app_test.IosAppTest,
+                            ios_web_test.TestWebsiteiOSSafari,
+                            android_app_test.AndroidAppTest,
+                            android_web_test.TestWebsiteAndroidChrome)
+
+if str(res.result()).find("errors=0") > -1 and str(res.result()).find("failures=0") > -1:
+    print("All tests passed!")
+    sys.exit(0)
+else:
+    print("Some tests failed!")
+    sys.exit(1)
+
